6628de25d4772b72bfb7e8294ad74c004fcbb4ff
MarySoroka <44846587+MarySoroka@users.noreply.github.com>
2019-10-14 22:52:18 +0300

    creation of the main form

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello");
+import tkinter
+from tkinter import *
+
+
+
+window = tkinter.Tk()
+window.title("Hasltead's metrics")
+window.geometry('800x500')
+lbl = Label(window, text="Your program")
+lbl.grid(column=0, row=0)
+btn = Button(window, text="Analyse")
+btn.grid(column=1, row=0)
+TextArea = tkinter.Text(window)
+
+window.mainloop()
+
