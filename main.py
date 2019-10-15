f363b9bb0bbb2af8da97065784fd5f176f6d4443
MarySoroka <44846587+MarySoroka@users.noreply.github.com>
2019-10-15 23:20:29 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	createTable.py
    #	main.py

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import tkinter
 from tkinter import *
 from tkinter import scrolledtext, filedialog, messagebox
 
+import self as self
 
-# set the value of checkbox and set te visibiality of button to open file
 from createTable import ExampleApp
 
+flag = 0
 
+
+# set the value of checkbox and set te visibiality of button to open file
 def setChoose():
     var.set(not var.get())
     if var.get():
@@ -18,25 +21,43 @@ def setChoose():
         openFileBtn.pack()
 
 
+def readFromTextbox():
+    if inputText.get('0.1') == '':
+        text = self.inputText.get('1.0', END).splitlines()
+        for line in text:
+            func(line.split())
+    else:
+        messagebox.showerror('Error', 'Please, choose your file or inter your text')
+
+
 # open dialog to find file and after that define variable fileName as path of the file
 # read from file by lines and write into text area in the main form
 def openFile():
     fileName = filedialog.askopenfilename(filetypes=(("TXT files", "*.txt"),))
     inputText.delete('1.0', END)
-    f = open(fileName, 'r')
-    numOfLine = 0
-
-    for line in f:
-        if numOfLine == 0:
-            line = line[1:]
-            line = line[:0] + ' ' + line[1:]
-        inputText.insert(END, line)
-        numOfLine = numOfLine + 1
+    name = fileName
+    try:
+        f = open(name, 'r')
+        numOfLine = 0
+        global flag
+        flag = 0
+        for line in f:
+            if numOfLine == 0:
+                line = line[1:]
+                line = line[:0] + ' ' + line[1:]
+            inputText.insert(END, line)
+            numOfLine = numOfLine + 1
+    except (OSError, IOError) as e:
+        messagebox.showerror('Error', 'Please, choose your file or inter your text')
 
 
 # show result table
 def showTable():
-   ExampleApp()
+    if inputText.get('0.1') == '':
+        messagebox.showerror('Error', 'Please, choose your file or inter your text')
+    else:
+        ExampleApp()
+        readFromTextbox()
 
 
 # addition of the all components in the main form and set their size
