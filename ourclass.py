c3af73416529a267961d1f4f91167e5b5ca95735
AngelSel <angelbox1@gmail.com>
2019-10-16 12:33:27 +0300

    c

--- ourclass.py
+++ ourclass.py
@@ -1,5 +1,5 @@
 class MyClass(object):
-    def __init__(self, name, amount, usability):
+    def __init__(self, name, initialization, usability):
         self.name = name
         self.initialization = initialization
         self.usability = usability
